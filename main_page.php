bc9f608e06c5b1dedc9dac5ed727cddf3b06e543
Kabir Virji <kabirvirji@gmail.com>
2016-07-08 23:24:17 -0700

    added dead links

--- main_page.php
+++ main_page.php
@@ -1,5 +1,13 @@
-<?php
+<!DOCTYPE html>
 
-echo "This is the first php file!";
-
-?>
+<html lang='en'>
+	<head>
+		<title>Main Page</title>
+	</head>
+	<body>
+		<a href="/student_register">Student Register</a><br>
+		<a href="/mentor_register">Mentor Register</a><br>
+		<a href="/student_login">Student Login</a><br>
+		<a href="/mentor_login">Mentor Login</a><br>
+	</body>
+</html>
