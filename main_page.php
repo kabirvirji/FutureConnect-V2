3b8154dc4b352282a1f0dc59b5a97904031a00ac
Kabir Virji <kabirvirji@gmail.com>
2016-07-09 18:52:39 -0700

    updated paths

--- main_page.php
+++ main_page.php
@@ -5,9 +5,9 @@
 		<title>Main Page</title>
 	</head>
 	<body>
-		<a href="/student_register">Student Register</a><br>
-		<a href="/mentor_register">Mentor Register</a><br>
-		<a href="/student_login">Student Login</a><br>
-		<a href="/mentor_login">Mentor Login</a><br>
+		<a href="/first-cms/student_register.php">Student Register</a><br>
+		<a href="/first-cms/mentor_register.php">Mentor Register</a><br>
+		<a href="/first-cms/student_login.php">Student Login</a><br>
+		<a href="/first-cms/mentor_login.php">Mentor Login</a><br>
 	</body>
 </html>
