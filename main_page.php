6640c9052e27be999a8cb329eea324389bc7f83b
Kabir Virji <kabirvirji@gmail.com>
2016-07-09 18:55:57 -0700

    added helper functions and header/footer

--- main_page.php
+++ main_page.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
+<?php include("../first-cms/header.php") ?>
 
-<html lang='en'>
-	<head>
-		<title>Main Page</title>
-	</head>
-	<body>
-		<a href="/first-cms/student_register.php">Student Register</a><br>
-		<a href="/first-cms/mentor_register.php">Mentor Register</a><br>
-		<a href="/first-cms/student_login.php">Student Login</a><br>
-		<a href="/first-cms/mentor_login.php">Mentor Login</a><br>
-	</body>
-</html>
+	<title>Main Page</title>
+		<a href="/first-cms/student_register.php">Student Register</a><br><br>
+		<a href="/first-cms/mentor_register.php">Mentor Register</a><br><br>
+		<a href="/first-cms/student_login.php">Student Login</a><br><br>
+		<a href="/first-cms/mentor_login.php">Mentor Login</a><br><br>
+<?php include("../first-cms/footer.php") ?>
+
