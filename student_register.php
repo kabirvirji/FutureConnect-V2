c6e253b6e083f59811c134647c5f4d8da46c8265
Kabir Virji <kabirvirji@gmail.com>
2016-07-09 19:45:15 -0700

    added form validation

--- student_register.php
+++ student_register.php
@@ -3,37 +3,19 @@
 require_once("database_connection.php");
 // require the helper functions one
 require_once("functions.php");
-?>
 
-<!-- HTML header -->
-<?php include("../first-cms/header.php"); ?>
-
-	<title>Student Register</title>
-	<p>Welcome to the Student Register page! You can register with a username and password below.</p>
-
-	<!-- sending the filename as $_POST request -->
-	<form action="student_register.php" method="post">
-		<!-- name is key in $_POST array -->
-	 	username<br>
-	 	<input type="text" name="username"><br>
-	 	password<br>
-	  	<input type="password" name="password"><br><br>
-	  	<input type="submit" name="submit" value="submit">
-	</form>
-
-	<?php
+$username_error = "";
+$password_error = "";
 
 
-		if (isset($_POST['submit'])) {
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			$username = mysql_real_escape_string($username);
-			$password = mysql_real_escape_string($password);
-		
+if (isset($_POST['submit'])) {
+	empty($_POST['username']) ? $username_error = "required field" : $username = test_input($_POST['username']);
+	empty($_POST['password']) ? $password_error = "required field" : $password = test_input($_POST['password']);
 
-		// query and write
+
+	if (!empty($username) && !empty($password)) {
 		$sql_write = "INSERT INTO students (username, password)
-				VALUES ('{$username}', '{$password}')";
+			VALUES ('{$username}', '{$password}')";
 
 		$result = mysql_db_query("FutureConnect", $sql_write);
 
@@ -42,8 +24,32 @@ require_once("functions.php");
 		if (!$result) {
 			echo "Database Query Failed";
 		}
-
+	}
 }
+
+?>
+
+<!-- HTML header -->
+<?php include("../first-cms/header.php"); ?>
+
+	<title>Student Register</title>
+	<p>Welcome to the Student Register page! You can register with a username and password below.</p>
+	<!-- sending the filename as $_POST request -->
+	<form action="student_register.php" method="post">
+		<!-- name is key in $_POST array -->
+	 	username<br>
+	 	<input type="text" name="username"><br>
+	 	<span class="error">* <?php echo $username_error;?></span><br>
+	 	password<br>
+	  	<input type="password" name="password"><br><br>
+	  	<span class="error">* <?php echo $password_error;?></span><br>
+	  	<input type="submit" name="submit" value="submit">
+	</form>
+
+	<?php
+
+
+
 	?>
 
 <!-- HTML footer -->
