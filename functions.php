d8821c8845fc177ed67fa6decfa7b35969b269d2
Kabir Virji <kabirvirji@gmail.com>
2016-07-16 21:42:40 -0700

    updated functions

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function test_input($data) 
 {
-    $data = trim($data);
+	$data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     $data = mysql_real_escape_string($data);
