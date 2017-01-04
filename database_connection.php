e35ae2934dda2a5b57f0b8c5c25ee314dc8363ac
Kabir Virji <kabirvirji@gmail.com>
2017-01-03 21:28:04 -0800

    security

--- database_connection.php
+++ database_connection.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "Hello666");
-define("DB_NAME", "FutureConnect");
+define("DB_HOST", "");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
+define("DB_NAME", "");
 
 $connection = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
